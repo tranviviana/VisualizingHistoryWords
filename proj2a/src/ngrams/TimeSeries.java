f2067ec01a3b40e4a99792f789745f5f3bfcd142
vivianatran <tranvivianaedu@gmail.com>
2023-10-06 01:03:03 -0700

    timeSeries works... restarting N gram

--- proj2a/src/ngrams/TimeSeries.java
+++ proj2a/src/ngrams/TimeSeries.java
@@ -42,8 +42,7 @@ public class TimeSeries extends TreeMap<Integer, Double> {
      */
     public List<Integer> years() {
         // TODO: Fill in this method.
-        List<Integer> yearsList = new ArrayList<>();
-        yearsList.addAll(navigableKeySet());
+        List<Integer> yearsList = new ArrayList<>(navigableKeySet());
         return yearsList;
     }
 
