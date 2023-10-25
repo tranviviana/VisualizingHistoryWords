d1c46a9f340accc357f81236b87ec83d7e503323
vivianatran <tranvivianaedu@gmail.com>
2023-10-24 21:49:36 -0700

    graph with three hashmaps

--- proj2b/src/main/wordrelationship/Graph.java
+++ proj2b/src/main/wordrelationship/Graph.java
@@ -2,9 +2,7 @@ package main.wordrelationship;
 
 import org.checkerframework.checker.units.qual.A;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
+import java.util.*;
 
 
 //ADJACENCY LIST APPROACH
@@ -12,53 +10,53 @@ import java.util.HashMap;
 //map id to word
 //map word to connection
 public class Graph<T> {
-    //connecting to children
-    private final int minimumID = 0;
-    private int maxmiumID;
-    /*childrenIds are a set of numbers that we are going to have to put in tandem with nodeNames to get the total list we are looking for*/
-    /*nodeNames looks at one node SPECIFICALLY*/
-    /*stringIds looks at the associated words and returns the ids that it shows up in*/
-    private ArrayList<Integer> childrenIds;
-    private ArrayList<String> nodeNames;
-    private ArrayList<Integer> stringIds;
+    private Collection<Integer> childrenIds;
     private HashMap<Integer, Collection<Integer>> adjacentChildren;
     //each "node" is a name
     private HashMap<Integer, Collection<String>> connectionToDefinition;
     private HashMap<String, Collection<Integer>> connectionToID;
 
-    public Graph(int size) {
-        adjacentChildren = new HashMap<>();
-        connectionToDefinition = new HashMap<>();
-        connectionToID = new HashMap();
-        maxmiumID = size - 1;
-    }
-    public String nameOfVertex(int id) {
-        return connectionToDefinition.get(id).toString();
-
-    }
-    public void addEdge(int id, int child) {
-        Collection<String> parentName = connectionToDefinition.get(id);
-        Collection<String> childName = connectionToDefinition.get(child);
-        Collection<String> currentChildren = adjacentChildren.remove(parentName);
-        currentChildren.add(childName.toString());
-        adjacentChildren.put(parentName, currentChildren);
+    public Graph() {
+        this.adjacentChildren = new HashMap<>();
+        this.connectionToDefinition = new HashMap<>();
+        this.connectionToID = new HashMap<>();
     }
 
-   /* ??????? how to add multiple children?????? */
-    public Collection<String> getChildren (String parent) {
-        throw new UnsupportedOperationException();
-    }
-    //need to alphabetize and what if the name is 2 words
-    private Collection<String> alphabetize(Collection<String> childrenUnordered, String parent) {
-        Collection<String> allWords = new ArrayList<>();
-        for (String word : childrenUnordered) {
-            allWords.add(word);
+    //adds the name of the word to accessible key
+    public void addDefinitionSingle(int id, String word) {
+        if (!connectionToDefinition.get(id).contains(word)) {
+            connectionToDefinition.get(id).add(word);
         }
-        allWords.add(parent);
-        return allWords;
+        addIDSingle(word, id);
     }
-    public boolean existenceOfWord (String word) {
-        return connectionToDefinition.containsValue(word);
+    //each word is connected to a certain id or multiple ids
+    //adds the string's id to its occurences
+    public void addIDSingle(String word, int id) {
+        if (!connectionToID.get(word).contains(id)) {
+            connectionToID.get(word).add(id);
+        }
+    }
+    //adds the name of WORDSSSS to accessible key
+    public void addDefinitionMultiple(int id, Collection<String> words) {
+        connectionToDefinition.put(id, words);
+        for (String word : words) {
+            addIDSingle(word, id);
+        }
+    }
+
+    private String getChildren(String parentNode) {
+        Collection<Integer> occurrences = connectionToID.get(parentNode);
+        Collection<Integer> totalIds = new ArrayList<>();
+        List<String> allWords = new ArrayList<>();
+        for (int sibling : occurrences) {
+            totalIds.addAll(adjacentChildren.get(sibling));
+        }
+        totalIds.addAll(occurrences);
+        for (int id : totalIds) {
+            allWords.addAll(connectionToDefinition.get(id));
+        }
+        Collections.sort(allWords);
+        return allWords.toString();
     }
 
 }
